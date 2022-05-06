38a43e2b815f2d1b67c39d72c928ee8c4b262a42
Samuel <sialiaga@miuandes.cl>
2022-05-05 23:22:10 -0400

    Fibonaci Logic Finish

--- Main.js
+++ Main.js
@@ -1,20 +1,38 @@
-const fs = require('fs');
 
-var num;
-var fibo_num;
+function fibonacci(number) {
+    if (number == 0) {
+        return 0
+    }
+    else if (number == 1) {
+        return 1
+    }
+    else {
+        return fibonacci(number-1) + fibonacci(number-2)
+    }
 
-fs.readFile("input.txt", 'utf8', (err, data) =>
-{
-    // if(err)
-    // {
-    //     console.error(err);
-    //     return;
-    // }
-    num = parseInt(data);
     
+}
+
+const fs = require('fs');
+var num = 0;
+var fibonacci_num;
+
+fs.readFile("input.txt", 'utf8', (err, data) => {
+    if(err)
+    {
+        console.error(err);
+        return;
+    }
+    
+    num = parseInt(data);
+    console.log("El numero en el archivo es... " + num)
+
+    fibonacci_num = fibonacci(num)
+    console.log("Su valor en la sucesion de Fibonacci es... F(" + num + ") = " + fibonacci_num)
+      
 });
 
-console.log("El numero seleccionado es: " + num)
+
 
 
 
